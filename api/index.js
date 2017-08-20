b87417745562a9ca1025906fbc5c5ac410c4b4f4
davidep87 <davidepolano@gmail.com>
2017-08-20 10:50:47 +0200

    minor update

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const router = require('koa-router')()
 const body = require('koa-body')()
-
 const { example } = require('./example')
 
 router
   .get('/api/example', example)
-
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const http2 = require('http2')
 const koa = require('koa')
-const api = require('koa-router')()
-const apiRoutes = require('./api/index')
+const api = require('./api/index')
 const config = require('./config/index')
 const app = new koa()
 
-api.use(apiRoutes.routes())
 app.use(api.routes())
 
 config.ssl.active? http2.createServer(config.ssl, app.callback()).listen(config.server.port) : app.listen(config.server.port)
